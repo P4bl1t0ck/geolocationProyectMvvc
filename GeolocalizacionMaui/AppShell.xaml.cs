644a1fde17a0953bed11b248ac99b040f8f0f353
laboratorio <laboratorio@UPOAULA10705>
2025-06-18 09:03:44 -0500

    Agregar archivos de proyecto.

---
+++ GeolocalizacionMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GeolocalizacionMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GeolocalizacionMaui/Repositories/WeatherServices.cs
@@ -0,0 +1,30 @@
+﻿using CoreImage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeolocalizacionMaui.Models;
+
+namespace GeolocalizacionMaui.Repositories
+{
+    
+    public class WeatherServices
+    {
+        public async Task<WeatherData> GetWeatherDataAsync( double latitude, double longitude)
+        {
+            string latitude_str = latitude.ToString().Replace(",",".");
+            string longitude_str = longitude.ToString().Replace(",", ".");
+            string url = $"https://api.open-meteo.com/v1/forecast?latitude={latitude_str}&longitude={longitude_str}&current=temperature_2m,relative_humidity_2m,rain";
+            //https://api.open-meteo.com/v1/forecast?latitude=46.9481&longitude=7.4474&current=temperature_2m,relative_humidity_2m,rain
+            //HttpClient httpClient = new HttpClient();
+            using (HttpClient client = new HttpClient()) 
+            {
+                var response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();//Dispara la excepcion numerica.
+                var result = await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+
+}
